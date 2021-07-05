69b290405de5d7000a9fccb2f0e2f20a1073b34a
unknown <mittervan@gmail.com>
2021-07-05 14:36:22 +0700

    thuc hanh khai bao bien

---
+++ src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    public static void main(String[] abc) {
+        int i = 10;
+        double d = 20.5;
+        float f = 20.5f;
+        boolean b = true;
+        char c = 'a';
+        String s = "Ha Noi";
+        System.out.println("i"+i);
+        System.out.println("d"+d);
+        System.out.println("f"+f);
+        System.out.println("b"+b);
+        System.out.println("c"+c);
+        System.out.println("s"+s);
+    }
+}
